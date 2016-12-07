3cda686852eb9082d735624306bd4940aa6a9ba7
Tom McTiernan <thomasmctiernan@gmail.com>
2016-12-07 22:47:34 +0000

    Add failing tests for harder cases

--- problems/1a/Solver1aTest.py
+++ problems/1a/Solver1aTest.py
@@ -1,12 +1,22 @@
 import unittest
-from .Solver1a import Solver1a
+from Solver1a import Solver1a
 
 
 class Solver1aTest(unittest.TestCase):
-    def test_upper(self):
+    def test_example_1(self):
         solver = Solver1a()
         shortest_path = solver.get_shortest_path("R2, L3")
         self.assertEqual(shortest_path, 5)
+
+    def test_example_2(self):
+        solver = Solver1a()
+        shortest_path = solver.get_shortest_path("R2, R2, R2")
+        self.assertEqual(shortest_path, 2)
+
+    def test_example_3(self):
+        solver = Solver1a()
+        shortest_path = solver.get_shortest_path("R5, L5, R5, R3")
+        self.assertEqual(shortest_path, 12)
 
 
 if __name__ == '__main__':
