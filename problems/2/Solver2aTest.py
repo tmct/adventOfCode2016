86e8bfa450f790ac8d286c4f37e66cdb7195f7d1
Tom McTiernan <thomasmctiernan@gmail.com>
2016-12-10 09:18:54 +0000

    add new,failing example

--- problems/2/Solver2aTest.py
+++ problems/2/Solver2aTest.py
@@ -6,12 +6,21 @@ class Solver2aTest(unittest.TestCase):
     def test_example(self):
         solver = Solver2a()
         # noinspection SpellCheckingInspection
-        shortest_path = solver.get_bathroom_code(
+        code = solver.get_bathroom_code(
             'ULL\n'
             'RRDDD\n'
             'LURDL\n'
             'UUUUD\n')
-        self.assertEqual(shortest_path, '1985')
+        self.assertEqual(code, '1985')
+
+    def test_my_example(self):
+        solver = Solver2a()
+        # noinspection SpellCheckingInspection
+        code = solver.get_bathroom_code(
+            'UDDU\n'
+            'RDDRDLL\n'
+            'URUR\n')
+        self.assertEqual(code, '573')
 
 
 if __name__ == '__main__':
