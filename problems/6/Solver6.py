9d42275c0d75873545a859b874e312f8ac241402
Tom McTiernan <thomasmctiernan@gmail.com>
2016-12-11 11:15:42 +0000

    Solve 6b

--- problems/6/Solver6.py
+++ problems/6/Solver6.py
@@ -2,6 +2,10 @@ def most_common(lst):
     return max(set(lst), key=lst.count)
 
 
+def least_common(lst):
+    return min(set(lst), key=lst.count)
+
+
 class Solver6:
     def __init__(self, input_file_name):
         with open(input_file_name, 'r') as message_file:
@@ -11,3 +15,8 @@ class Solver6:
         columns = list(''.join(x) for x in zip(*self.message_file_content))[:-1]
         most_common_chars = (most_common(x) for x in columns)
         return ''.join(most_common_chars)
+
+    def get_message_using_modified_code(self):
+        columns = list(''.join(x) for x in zip(*self.message_file_content))[:-1]
+        least_common_chars = (least_common(x) for x in columns)
+        return ''.join(least_common_chars)
--- problems/6/Solver6Test.py
+++ problems/6/Solver6Test.py
@@ -15,5 +15,17 @@ class Solver6Test(unittest.TestCase):
         message = solver.get_message()
         self.assertEqual('umcvzsmw', message)
 
+    def test_example_input_b(self):
+        input_file = 'test_input.txt'
+        solver = Solver6(input_file)
+        message = solver.get_message_using_modified_code()
+        self.assertEqual('advent', message)
+
+    def test_real_input_b(self):
+        input_file = 'input.txt'
+        solver = Solver6(input_file)
+        message = solver.get_message_using_modified_code()
+        self.assertEqual('rwqoacfz', message)
+
 if __name__ == '__main__':
     unittest.main()
